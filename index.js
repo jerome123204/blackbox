468b23e30aa2dbae60a17e8cfe701350d21b96c7
jerome123204 <154974042+jerome123204@users.noreply.github.com>
2024-02-01 10:52:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 6969;
+const port = 2000;
 app.use(express.json());
 
 app.get('/ask', (req, res) => {
